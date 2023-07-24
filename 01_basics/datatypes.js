5ae9a610c91aa1dfcc3b494a860350ad4a53ab54
Reshika <104939276+Reshika-Tamang@users.noreply.github.com>
2023-07-24 13:26:06 +0000

    datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"  // treat all JS  code as newer version or newer JS, will treat the code as code as new stats by the engine
+//alert(3+3) // we are using node js and not the browser
+//  javascript engine is hidden in browser
+console.log(3+3)
+console.log("Tamang@01")
+let name ="Reshika"
+let age = 24
+let islogged = false
+let state
+
+//number - 2 to power 53
+//strings - ""
+//bool = true or false
+//null = standalone value
+//undefined
+//symbol - used in react mostly where we talk about different components
+
+//object 
+
+console.log(typeof null)
+// Type of null is an  object but type of undefined is undefined
---
+++ 01_basics/vari.js
@@ -0,0 +1,11 @@
+const AccId = 1234
+let acc_Email = "reshika@1999"
+var password = "1234"
+account_CITY = "Jaipur"
+
+//AccId = 012//not allowed
+/*java  script didnt  work in scope ,prefer not to use var because of issue in block scope and functional scope*/
+acc_Email = "Tamang@01"
+console.log(AccId)
+console.log(acc_Email)
+console.table([AccId,password,account_CITY])
