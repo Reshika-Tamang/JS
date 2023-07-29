92420db200e8f89dbdbb8835ed42bd8e1a7cdd66
Reshika <104939276+Reshika-Tamang@users.noreply.github.com>
2023-07-29 08:28:42 +0000

    Updated summary  along with use of memory

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -25,3 +25,26 @@ const my_func = function(){
 }
 
 
+
+//*********************************************************************** *//
+//Memory : Stack (primitive),Heap(Non Primitive)
+
+let name = "Reshika"
+let name2 = name
+name2 = "Tamang"
+
+//since this is primitive  datatype thats why its using stack as memory and it only gives call by value
+console.log(name2)
+
+let my_info ={
+   name: "Reshika Tamang",
+   Age: 24,
+}
+
+let myinfo2 = my_info
+myinfo2.name = "Abcde"
+console.log(myinfo2.name)
+
+//Since this is non primitive datatype and since non primitive datatype uses heap and gives references rather than value thats y its changing myinfo too
+console.log(my_info.name)
+
