23f202fb5df1ac0695f252cb63b8d2ea616748fb
Reshika <104939276+Reshika-Tamang@users.noreply.github.com>
2023-07-29 06:01:26 +0000

    datatype comparision

---
+++ 01_basics/dtcomp.js
@@ -0,0 +1,12 @@
+console.log("2" > 1)
+console.log("02" > 1)// convert 2 to number datatypes
+//Typescript does not allow to compare two different datatypes
+
+console.log(null == 0)
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null <= 0)//this is not to be done  and for undefined also 
+//In JS comparision and equality cheeck are two different things
+/// === is strict check that checks not only the values but also the datatype
+
+//console.log("2" === 2)
